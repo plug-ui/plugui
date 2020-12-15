af54bd6f2c2d8dc3311882b63f56d893e3ae4e5c
Prakhar Jaiman <jaimanprakhar12345@gmail.com>
2020-12-15 21:36:44 +0530

    documentation

---
+++ docs/src/examples/dropdown/Dropdown.vue
@@ -0,0 +1,219 @@
+<template>
+  <div>
+      <div class="pl-dropdown--1 pl-ml--4 pl-mr--4">
+          <button class="pl-dropdown--heading">Select Options <i class="light-icon-caret-down"></i></button>
+          <ul>
+              <li><a href="#">Options 1</a></li>
+              <li><a href="#">Options 2</a></li>
+              <li><a href="#">Options 3</a></li>
+              <li><a href="#">Options 3</a></li>
+          </ul>
+      </div>
+      <div class="pl-dropdown--2 pl-ml--4 pl-mr--4">
+          <button class="pl-dropdown--heading">Select Options <i class="light-icon-caret-down"></i></button>
+          <ul>
+              <li><a href="#">Options 1</a></li>
+              <li><a href="#">Options 2</a></li>
+              <li><a href="#">Options 3</a></li>
+              <li><a href="#">Options 3</a></li>
+          </ul>
+      </div>
+      <div class="pl-dropdown--3 pl-ml--4">
+          <button class="pl-dropdown--heading">Select Options</button>
+          <ul>
+              <li><a href="#">Options 1</a></li>
+              <li><a href="#">Options 2</a></li>
+              <li><a href="#">Options 3</a></li>
+              <li><a href="#">Options 3</a></li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.pl-dropdown--1 {
+    font-family: sans-serif;
+    position: relative;
+    display: inline-block;
+    .pl-dropdown--heading {
+        border: 1px solid #a4adb8;
+        outline: none;
+        border-radius: 5px;
+        background: #D7DFE8;
+        padding: 5px 0.6rem 5px 1rem;
+        color: #566D79;
+        font-size: 16px;
+        cursor: pointer;
+        &:focus + ul {
+            opacity: 1;
+            pointer-events: all;
+            transform: translateY(0);
+        }
+        i {
+            font-size: 12px;
+            color:#566D79;
+        }
+    }
+    ul {
+        width: 200px;
+        position: absolute;
+        margin-top: 3px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        list-style: none;
+        border-radius: 5px;
+        transform: translateY(-10px);
+        opacity: 0;
+        pointer-events: none;
+
+        transition: all 0.3s ease-in-out;
+        li {
+            background:#D7DFE8;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color:#566D79;
+            padding: 6px 0;
+            cursor: pointer;
+            &:hover{
+                background: #cbd6e2;
+            }
+            a {
+                color:#566D79;
+                text-decoration: none;
+            }
+        }
+        }
+    }
+.pl-dropdown--2 {
+    font-family: sans-serif;
+    position: relative;
+    display: inline-block;
+    .pl-dropdown--heading {
+        border: none;
+        outline: none;
+        border-radius: 5px;
+        background: #D7DFE8;
+        padding: 5px 0.6rem 5px 1rem;
+        color: #566D79;
+        font-size: 16px;
+        cursor: pointer;
+        &:focus {
+            border-bottom-left-radius: 0px;
+            border-bottom-right-radius: 0px;
+        }
+        &:focus + ul {
+            opacity: 1;
+            pointer-events: all;
+            transform: translateY(0);
+        }
+        i {
+            font-size: 12px;
+            color:#566D79;
+        }
+    }
+    ul {
+        width: 100%;
+        position: absolute;
+        margin-top: 0px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        list-style: none;
+        border-radius: 5px;
+        transform: translateY(-10px);
+        opacity: 0;
+        pointer-events: none;
+        transition: all 0.3s ease-in-out;
+        li {
+            background:#f3f4f7;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color:#566D79;
+            padding: 6px 0;
+            cursor: pointer;
+            &:hover{
+                background:#dfe0e2;
+            }
+            a {
+                color:#566D79;
+                text-decoration: none;
+            }
+        }
+        }
+    }
+.pl-dropdown--3 {
+    font-family: sans-serif;
+    position: relative;
+    display: inline-block;
+    width: 200px;
+    .pl-dropdown--heading {
+        text-align: left;
+        width: 100%;
+        border: none;
+        outline: none;
+        border-radius: 2px;
+        background: #D7DFE8;
+        padding: 5px 0.6rem 5px 1rem;
+        color: #566D79;
+        font-size: 16px;
+        cursor: pointer;
+        &:focus + ul {
+            opacity: 1;
+            pointer-events: all;
+            transform: translateY(0);
+        }
+        i {
+            font-size: 12px;
+            color:#566D79;
+        }
+    }
+    ul {
+        width: 100%;
+        position: absolute;
+        margin-top: 0px;
+        // display: flex;
+        flex-direction: column;
+        // justify-content: space-around;
+        // align-items: center;
+        list-style: none;
+        border-radius: 5px;
+        transform: translateY(-15px);
+        opacity: 0;
+        pointer-events: none;
+        transition: all 0.5s ease-in-out;
+        li {
+            background:#f3f4f7;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            // justify-content: center;
+            // align-items: center;
+            color:#566D79;
+            padding: 6px 0 6px 15px;
+            cursor: pointer;
+            &:hover{
+                background:#dfe0e2;
+            }
+            a {
+                color:#566D79;
+                text-decoration: none;
+            }
+        }
+        }
+    }
+</style>
--- docs/src/pages/start.vue
+++ docs/src/pages/start.vue
@@ -11,9 +11,59 @@
     <br />
     <div class="content-section implementation">
       <docs-card title="Installation Guide" :responsive="false">
-        <div class="pl-info-msg">
-          <i class="light-icon-book"></i>
-          <p>Installation docs will be updated soon...</p>
+        <div class="pl-info-msg ">
+          <!-- <i class="light-icon-book"></i> -->
+          <div class="pl-docs">
+            <h4>
+              Installing and Using PlugUI is simple and can be done the
+              following ways
+            </h4>
+            <ol>
+              <li>Use <b>npm</b> and install the plugui package</li>
+              <li>Use our <b>github</b> repository and get the latest</li>
+              <li>Use the help of <b>CDN</b></li>
+            </ol>
+            <div class="pl-methods">
+              <h3>Use the HTML5 doctype</h3>
+              <pre><code data-lang="html"><span class="">&lt;!DOCTYPE html&gt;</span></code></pre>
+            </div>
+            <div class="pl-methods">
+              <h3>Use the jsDelivr CDN</h3>
+              <a href="https://www.jsdelivr.com/package/npm/plugui"
+                >https://www.jsdelivr.com/package/npm/plugui</a
+              >
+            </div>
+            <div class="pl-methods">
+              <h3>Download from the repository</h3>
+              <a href="https://github.com/plug-ui/plugui"
+                >https://github.com/plug-ui/plugui</a
+              >
+            </div>
+            <div class="pl-methods">
+              <h3>Starter Template</h3>
+              <pre><code class="language-html" data-lang="html"><span class="cp">&lt;!DOCTYPE html&gt;</span>
+<span class="">&lt;html&gt;</span>
+  <span class="">&lt;head&gt;</span>
+    <span class="">&lt;meta</span> <span class="na">charset=</span><span class="">"utf-8"</span><span class="">&gt;</span>
+    <span class="">&lt;meta</span> <span class="na">name=</span><span class="">"viewport"</span> <span class="na">content=</span><span class="">"width=device-width, initial-scale=1"</span><span class="">&gt;</span>
+    <span class="">&lt;title&gt;</span>Hello Bulma!<span class="">&lt;/title&gt;</span>
+    <span class="">&lt;link</span> <span class="na">rel=</span><span class="">"stylesheet"</span> <span class="na">href=</span><span class="">"https://cdn.jsdelivr.net/npm/plugui"</span><span class="">&gt;</span>
+  <span class="">&lt;/head&gt;</span>
+  <span class="">&lt;body&gt;</span>
+  <span class="">&lt;section</span> <span class="na">class=</span><span class="">"section"</span><span class="">&gt;</span>
+    <span class="">&lt;div</span> <span class="na">class=</span><span class="">"container"</span><span class="">&gt;</span>
+      <span class="">&lt;h1</span> <span class="na">class=</span><span class="">"title"</span><span class="">&gt;</span>
+        Hello World
+      <span class="">&lt;/h1&gt;</span>
+      <span class="">&lt;p</span> <span class="na">class=</span><span class="">"subtitle"</span><span class="">&gt;</span>
+        My first website with <span class="">&lt;strong&gt;</span>PLUGUI<span class="">&lt;/strong&gt;</span>!
+      <span class="">&lt;/p&gt;</span>
+    <span class="">&lt;/div&gt;</span>
+  <span class="">&lt;/section&gt;</span>
+  <span class="">&lt;/body&gt;</span>
+<span class="">&lt;/html&gt;</span></code></pre>
+            </div>
+          </div>
         </div>
       </docs-card>
       <div class="pl-warning-msg pl-shadow__exp-2">
@@ -32,18 +82,20 @@
         :responsive="false"
       >
         <div class="pl-success-msg">
-          <i class="light-icon-brand-github"></i>
-          <p>
-            Contribution docs will be updated soon...
-            <br />
-            Meanwhile, feel free to contribute directly on the
-            <a
-              href="https://github.com/plug-ui/plugui"
-              target="_blank"
-              class="info-link"
-              >GitHub Repository</a
-            >
-          </p>
+          <!-- <i class="light-icon-brand-github"></i> -->
+          <div class="pl-docs">
+            <h3>
+              In order to contribute to plugui, you can install its dependencies
+              and can preview its docs with hot reload at local host:3000
+            </h3>
+
+            PLUG UI is a collection of CSS only Pluggable and Lightweight UI
+            Elements
+            <h6># Install dependencies</h6>
+            <pre><code data-lang="html"><span class="">$ npm install</span></code></pre>
+            <h6># Preview docs with hot reload at localhost:3000</h6>
+            <pre><code data-lang="html"><span class="">$ npm run dev</span></code></pre>
+          </div>
         </div>
       </docs-card>
     </div>
@@ -54,15 +106,49 @@
 import DocsCard from "@/collections/docs-card/DocsCard.vue";
 export default {
   components: {
-    DocsCard,
-  },
+    DocsCard
+  }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .info-link {
   color: #3e8635;
   font-weight: 600;
   // text-decoration: underline;
+}
+.pl-docs {
+  padding: 10px 0 0 10px;
+  display: flex;
+  flex-direction: column;
+  ol {
+    padding: 10px 20px;
+    line-height: 2rem;
+  }
+  h3 {
+    color: #333333;
+    margin-bottom: 10px;
+  }
+  .pl-methods {
+    margin-bottom: 30px;
+  }
+  pre {
+    display: block;
+    width: 100%;
+    background-color: #f5f5f5;
+    border-radius: 6px;
+    color: #586e75;
+    padding: 10px;
+  }
+  a {
+    font-size: 18px;
+    font-style: none;
+    color: blue;
+    font-weight: bold;
+  }
+  h6 {
+    font-size: 16px;
+    margin-top: 10px;
+  }
 }
 </style>
